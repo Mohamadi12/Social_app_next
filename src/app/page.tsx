8ec498d1be679e4e8cc6483c65fc9d4b0e9339d0
Nana Mohamadi <adilnana78@gmail.com>
2025-05-01 12:35:45 +0000

    Building Navbar.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-import ModeToggle from "@/components/modeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import React from "react";
+import React from 'react'
 
 const Home = () => {
   return (
-    <div className="mt-8">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button variant="secondary">Click Me</Button>
-    </div>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
